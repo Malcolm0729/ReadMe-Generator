1ba8245685ee2df34f36e7f4294ccd8d8f7eb0db
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-02-28 23:23:51 -0800

    fixing the index js to make it presentable and hopefully getting it to run

--- index.js
+++ index.js
@@ -1,87 +1,52 @@
-const inquirer = require('inquirer')
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 const fs = require('fs');
-const path = require('path')
+const questions = require('./utils/questions');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-const questions = [
-    {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email',
-    },
-    {
-        type: 'input',
-        message: 'What is your github username?',
-        name: 'usernname',
-    },
-    {
-        type: 'input',
-        message: 'What is the title of your ptoject?',
-        name: 'title',    
-    },
-    {
-        type: 'input',
-        message:'Type a descriptive overview of the project goal',
-        name: "description",
-    },
-    {
-        type: 'list',
-        message: 'Which licensing is included in your project?',
-        name: 'license',
-        choices: ['MIT', 'APACHE 2.0', 'ISC', 'NONE'],
-    },
-    {
-        type: 'input',
-        message: 'What is the projects command',
-        default: 'npm i',
-    },
-    {
-        type: 'input',
-        messge: 'What is the projects run command?',
-        name: 'runCmd',
-        default: 'node index.js',
-    },
-    {
-        type: 'input',
-        message: 'How do you contribute to this project?',
-        name: 'contribute',
-    },
 
-];
 
-const makeUsageBullets = async (inputs = []) => {
-    constsetupPrompts = [
-        {
-            type: 'input',
-            message: 'Add a setup instuction bullet',
-            name: 'usageBullet',
-        },
-        {
-            type: 'confirm',
-            message: 'would you like to add another?',
-            name: 'repeat',
-            default: 'true',
-        },
-    ];
+// TODO: Create an array of questions for user input
 
-    const { repeat, ...answers } = await inquirer.prompt(setupPrompts);
-    const newInputs = [...inputs];
-    for (const ans in answers) {
-        newInputs.push(answers[ans]);
-    }
-    return repeat ? makeUsageBullets(newInputs) : newInputs;
+// refer to line 3, questions array is required from another file
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    // using file system writeFile method to rewrite the REAMME.md everytime the function is ran
+    fs.writeFile(
+        fileName, 
+        generateMarkdown( data ), 
+        (err) =>
+        err ? console.error(err) : console.log('Success!')
+    )};
+
+
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+        // prompts user using questions array we locally required
+        .prompt(questions)
+
+        // then uses the answers as data to use in our custom writeToFile function
+        .then((answers) => {
+            
+          // names file and selects where it is saved, gives data parameter set as users answers from prompt
+            writeToFile('./output/README.md', answers);
+            
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+        console.log(error)
+    } else {
+      // Something else went wrong
+        console.log(error)
+    };
+});
 };
 
-inquirer
-.prompt(prompts)
-.then(async (answers) => {
-    const next = await makeUsageBullets();
-    answers.usageBullets = next;
-    const mdText = makeMarkdown(answers);
-
-    fs.writeFile('README.md', mdText, (err)
-    err ? console.log(err) : console.log('Successfully created readme!!!')
-    );
-})
-.catch((err) => console.error(err));
-
+// Function call to initialize app
+init();
